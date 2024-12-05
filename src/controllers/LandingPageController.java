54d135ea1d7c277a4a1decf8329c778de4fa8feb
mcbaer23812 <mcbaer23812@gmail.com>
2024-12-04 21:47:55 -0500

    Added menu bar, buttons, CTA label, and footer

--- src/controllers/LandingPageController.java
+++ src/controllers/LandingPageController.java
@@ -1,18 +1,58 @@
 package controllers;
 
+import java.net.URI;
+import java.awt.Desktop;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 public class LandingPageController {
 
     @FXML
+    private Button btnAssessment;
+
+    @FXML
+    private Button btnLearnMore;
+
+    @FXML
+    private Label callToActionLabel;
+
+    @FXML
+    private Label createdByLabel;
+
+    @FXML
+    private Label githubLabel;
+
+    @FXML
     private GridPane landingPagePane;
+
+    @FXML
+    private ImageView logoImage;
 
     @FXML
     private Label logoLabel;
 
     @FXML
+    private MenuBar menuBar;
+
+    @FXML
     private Label taglineLabel;
+
+    @FXML
+    private Label versionLabel;
+
+    @FXML
+    void openGitHub(MouseEvent event) {
+          try {
+            String url = "https://github.com/mcbaer23812/StrokePredictor"; // Replace with your GitHub link
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (Exception e) {
+            e.printStackTrace(); // Handle exceptions
+        }
+    }
 
 }
